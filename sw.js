cb9119c8c15a1e96eb42fd343cba1df5a2dff101
Omar Essayeh <oe555@icloud.com>
2026-03-23 23:46:27 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'hapticwear-v1';
+const FILES = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
